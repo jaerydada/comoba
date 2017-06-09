75408269308bc0a22e6cc59a7064a2d37b24e76b
user1 <user1@user1-PC>
2017-06-09 17:35:21 +0900

    연습1

---
+++ springGITTest1/src/main/java/com/spring/springGITtest1/SpringGITtest1Controller.java
@@ -0,0 +1,21 @@
+package com.spring.springGITtest1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+
+import com.spring.springGITtest1.service.SpringGITtest1Service;
+
+//===== #32. 컨트롤러 선언 =====
+@Controller
+@Component
+//XML 에서 빈을 만드는 대신에 클래스명 앞에 @Component 어노테이션을 적어주면 해당 클래스는 
+//bean으로 자동 등록된다. 그리고 bean의 이름(첫글자는 소문자)은 해당 클래스 명이 된다.
+//그럼 여기서는 boardController 일듯
+public class SpringGITtest1Controller {
+
+//	===== #33. 의존객체 주입하기(DI : Dependency Injection) ===== 
+    @Autowired
+	private SpringGITtest1Service service;
+    
+}
